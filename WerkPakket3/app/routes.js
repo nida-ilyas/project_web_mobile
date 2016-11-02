385c9c207c465a23ee2d9fe013fc73b88fd95d38
nida-ilyas <nida.ilyas@student.pxl.be>
2016-11-02 01:52:51 +0100

    Routes

---
+++ WerkPakket3/app/routes.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { IndexRoute, Route } from 'react-router';
+import {
+    Habit3,
+    IngaveOverzicht,
+    insertCalories,
+    insertWeight,
+    overzichtPerHabit,
+}   from './components';
+
+export default (store) => {
+    return(
+        <Route path= "/" component={App}>
+        { /* Home (main) route */ }
+            <IndexRoute component ={Home} />
+
+        { /* Catch all route */ }
+        <Route path="*" component={NotFound} status={404} />
+        </Route>
+    );
+};
