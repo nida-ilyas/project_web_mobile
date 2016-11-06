c6fa72837707473582fa0612a989fc125b0c3185
wasla <Wasla.Habib@student.pxl.be>
2016-11-06 21:20:05 +0100

    Habit3Component

--- WerkPakket3/main.jsx
+++ WerkPakket3/main.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { render } from 'react-dom';
-import Dashboard from './app/components/DashboardComponent_wasla.jsx'
+//import Dashboard from './app/components/DashboardComponent_wasla.jsx'
+import Habits3 from './app/components/Habit3Component.jsx'
 
 
 //render(<WelcomeComponent />, document.getElementById('container'));
-render(<Dashboard />, document.getElementById('container'));
+//render(<Dashboard />, document.getElementById('container'));
+render(<Habits3 />, document.getElementById('container'));
