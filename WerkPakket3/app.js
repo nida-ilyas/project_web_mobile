645ec701b3f9b618a7cf749d4cd2fdb691bf5270
wasla <Wasla.Habib@student.pxl.be>
2016-11-06 12:56:05 +0100

    Merge branch 'master' of https://github.com/nida-ilyas/project_web_mobile
    # Conflicts:
    #	WerkPakket3/package.json

--- WerkPakket3/app.js
+++ WerkPakket3/app.js
@@ -4,3 +4,16 @@
 // holds the configuration for our expres app
 //
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+app.get('/', function(req, res, next) {
+    // Handle the get for this route
+});
+
+app.post('/', function(req, res, next) {
+    // Handle the post for this route
+});
