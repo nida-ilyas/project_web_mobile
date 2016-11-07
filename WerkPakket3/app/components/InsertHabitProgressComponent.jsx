6f71d1b5b44ea9f89b4e1715bccc5798d0cbd556
nida-ilyas <nida.ilyas@student.pxl.be>
2016-11-07 17:28:12 +0100

    tot hier toe: Insert Habit prog

--- WerkPakket3/app/components/InsertHabitProgressComponent.jsx
+++ WerkPakket3/app/components/InsertHabitProgressComponent.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
 import Request  from 'superagent';
 import _    from 'lodash';
-import ReactBootstrap from 'react-bootstrap';
-
-var FormGroup = ReactBootstrap.FormGroup;
+import FormGroup from 'react-bootstrap/lib/FormGroup';
+import ControlLabel from 'react-bootstrap/lib/ControlLabel';
+import FormControl from 'react-bootstrap/lib/FormControl';
+import Button from 'react-bootstrap/lib/Button';
+/*
+import Button from 'react-bootstrap/lib/Button';
+// or
+import { Button } from 'react-bootstrap';
+*/
+var reportHabit3;
 
 class InsertHabitProgressComponent extends React.Component{
+
     constructor(){
         super();
         this.state = {};
+
     }
     componentWillMount()
     {
@@ -18,21 +27,38 @@ class InsertHabitProgressComponent extends React.Component{
         {
             this.setState( // that wil cause the render method to cause again
                 {
+
                     habit1: response.body.Habit1,
                     habit2: response.body.Habit2,
                     habit3: response.body.Habit3
-
-
-
-
                 });
         });
 
     }
 
+    handleClick() {
+        //alert("I am an alert box!");
+        alert(reportHabit3);
+    }
+
+
+    handleChange(event){
+        reportHabit3 = (event.target.value);
+        return reportHabit3;
+    }
+
+
 
     render()
     {
+        // current date
+        var currentTime = new Date();
+        var month = currentTime.getMonth() + 1;
+        var day = currentTime.getDate();
+        var year = currentTime.getFullYear();
+        var d = month + "/" + day + "/" + year;
+
+
         var habit1 = this.state.habit1;
         var habit2 = this.state.habit2;
         var habit3 = this.state.habit3;
@@ -44,27 +70,23 @@ class InsertHabitProgressComponent extends React.Component{
 
         return  <div>
 
-            <input ref="textBox" type="text"/>
-            <h2>Habits </h2>
+            <h2>Invoer: {d}</h2>
             <ul>
                 <li>{habit1}</li>
                 <li>{habit2}</li>
                 <li>{habit3}</li>
             </ul>
 
-            <FormGroup controlId="formControlsSelect">
-                <ControlLabel>{habit1}</ControlLabel>
-                <FormControl componentClass="select" placeholder="select">
-                    <option value="select">Uitgevoerd</option>
-                    <option value="other">Niet uitgevoerd</option>
-                </FormControl>
-            </FormGroup>
 
+            <label>{habit3}</label>
+            <select onChange={this.handleChange.bind(this)}>
+                <option value="habit3NotDone">Niet uitgevoerd</option>
+                <option value="habit3Done">Uitgevoerd</option>
+            </select>
+
+            <Button onClick={this.handleClick} bsStyle="primary" >Submit</Button>
 
         </div>
     }
-
-
-
 }
 export  {InsertHabitProgressComponent as default};
--- WerkPakket3/main.jsx
+++ WerkPakket3/main.jsx
@@ -1,25 +1,28 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-<<<<<<< HEAD
-//import Dashboard from './app/components/DashboardComponent_wasla.jsx';
-//import Habits3 from './app/components/Habit3Component.jsx';
-//import Dashboard from './app/components/DashboardComponent.jsx';
-//import Habits3 from './app/components/Habit3Component.jsx';
-//import InsertWeight from './app/components/InsertWeightComponent.jsx';
-import InsertHabitProgressComponent from './app/components/InsertHabitProgressComponent.jsx';
+/* === DASHBOARD === */
+/*
+    import Dashboard from './app/components/DashboardComponent.jsx';
+    render(<Dashboard />, document.getElementById('container'));
+*/
 
-//render(<WelcomeComponent />, document.getElementById('container'));
-//render(<Dashboard />, document.getElementById('container'));
-//render(<Habits3 />, document.getElementById('container'));
-//render(<InsertWeight />, document.getElementById('container'));
-render(<InsertHabitProgressComponent/>, document.getElementById('container'));
-=======
-//import Dashboard from './app/components/DashboardComponent_wasla.jsx'
-//import Habits3 from './app/components/Habit3Component.jsx'
 
-//render(<WelcomeComponent />, document.getElementById('container'));
-//render(<Dashboard />, document.getElementById('container'));
-render(<Habits3 />, document.getElementById('container'));
-//render(<InsertWeight />, document.getElementById('container'));
->>>>>>> origin/master
+/* === Overzicht_3Habits === */
+/*
+    import Habits3 from './app/components/Habit3Component.jsx';
+    render(<Habits3 />, document.getElementById('container'));
+*/
+
+
+/* === InsertWeight === */
+/*
+ import InsertWeight from './app/components/InsertWeightComponent.jsx';
+ render(<InsertWeight />, document.getElementById('container'));
+*/
+
+
+/* === InsertHabitProgressComponent === */
+
+    render(<InsertHabitProgressComponent/>, document.getElementById('container'));
+    import InsertHabitProgressComponent from './app/components/InsertHabitProgressComponent.jsx';
